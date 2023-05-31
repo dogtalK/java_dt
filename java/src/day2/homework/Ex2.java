4eddf67642a1b0c6034013ebba22274c6d7ae69d
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-05-31 17:19:37 +0900

    자바 : 2번숙제

--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,9 +1,33 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("정수1을 입력해주세요.");
+		int num1 = sc.nextInt();
+		
+		System.out.println("정수2을 입력해주세요.");
+		int num2 = sc.nextInt();
+		
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / num2; // 1/2의 결과가 0이어서, 1.0/2로 바꾸어 0.5가 나오게 함
+		int mod = num1 % num2;
+		
+		
+		System.out.println("" + num1 + "+" + num2 + "=" + sum);
+		System.out.println("" + num1 + "-" + num2 + "=" + sub);
+		System.out.println("" + num1 + "*" + num2 + "=" + mul);
+		System.out.println("" + num1 + "/" + num2 + "=" + div);
+		System.out.println("" + num1 + "%" + num2 + "=" + mod);
+		
+		sc.close();
 
 	}
 

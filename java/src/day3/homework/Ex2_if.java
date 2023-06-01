307ee64cd4e36038f22759f6c638e50d7006a9ac
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-01 14:57:51 +0900

    자바 : 3일차 숙제

---
+++ java/src/day3/homework/Ex2_if.java
@@ -0,0 +1,21 @@
+package day3.homework;
+
+public class Ex2_if {
+	
+	/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용
+	(package : day3.homework, file : Ex2_if) 
+	input gender : 
+	M
+	Are you a women? man
+	
+	input gender : 
+	W
+	Are you a women? woman
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

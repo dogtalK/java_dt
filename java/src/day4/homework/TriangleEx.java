3900ad4781697135fbd9adc6030c9adfe6f75d1a
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-02 15:19:39 +0900

    자바 : 4일차 숙제3

---
+++ java/src/day4/homework/TriangleEx.java
@@ -0,0 +1,25 @@
+package day4.homework;
+
+import java.util.Scanner;
+
+public class TriangleEx {
+
+	public static void main(String[] args) {
+		/* 세 변의 길이를 입력 받아 세 변으로 삼격형을 만들 수 있는지 확인하는 코드를 작성하세요.
+		 * 세 변 중 작은 두변의 길이의 합이 큰 변보다 커야 삼각형을 만들 수 있다. */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("input num1");
+		int num1 = sc.nextInt();
+		System.out.println("input num2");
+		int num2 = sc.nextInt();
+		System.out.println("input num3");
+		int num3 = sc.nextInt();
+		
+		
+		
+		sc.close();
+	}
+
+}

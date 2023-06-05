818215fd4a5644c4e8f256dc85bb3b1d3dd52810
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-05 15:01:24 +0900

    자바 : 5일차  while문 구구단 예제

---
+++ java/src/day5/practice/MultiplicationTableEx.java
@@ -0,0 +1,16 @@
+package day5.practice;
+
+public class MultiplicationTableEx {
+
+	public static void main(String[] args) {
+		// 구구단 2단을 출력하는 코드를 작성하세요.
+		
+		int i = 1;
+		while(i <= 9) {
+			System.out.println(i*2);
+			i++;
+		}
+
+	}
+
+}

751ee4840fd5c8ae33ff2694bf7e598d6017304b
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-05 15:20:42 +0900

    자바 : 5일차 숙제

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -18,7 +18,15 @@ public class TeamLeaderEx {
 		int min = 1, max = 30;
 		int random = (int)(Math.random() * (max - min +1) + min);
 		
+		System.out.println(random);
 		
+		switch(random % 3) {
+		case 0:
+			System.out.println("you are team reader");
+			break;
+		case 1: case 2:
+			System.out.println("you are team member");
+		}
 
 	}
 
--- java/src/day5/homework/UpDownEx.java
+++ java/src/day5/homework/UpDownEx.java
@@ -1,5 +1,7 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class UpDownEx {
 
 	public static void main(String[] args) {
@@ -12,5 +14,27 @@ public class UpDownEx {
 		 * input : 45
 		 * Good!
 		 */
+		Scanner sc = new Scanner(System.in);
+		
+		int min = 1, max = 100;
+		int random = (int)(Math.random() * (max - min +1) + min);
+		System.out.println(random);
+		
+		int num = 0;
+		while(random != num) {
+			System.out.println("input number 1~100");
+			num = sc.nextInt();;
+			if(random > num && num >= 1 && num < 101) {
+				System.out.println("Up!");
+			}else if(random < num && num >= 1 && num < 101) {
+				System.out.println("Down!");
+			}else if(random == num) {
+				System.out.println("Good");
+			}else {
+				System.out.println("wrong number");
+			}
+		}
+		
+		sc.close();
 	}
 }

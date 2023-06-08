0a89ed8ceec8a22a322c5ab810b4ab078a61a021
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-08 13:51:27 +0900

    자바 : 7일차 중첩반복문 숙제

---
+++ java/src/day7/homework/StarEx5.java
@@ -0,0 +1,37 @@
+package day7.homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+
+		int i, j;
+		
+		for(i = 1; i <= 5; i++) {
+			for(j = 1; j<=5-i; j++) {
+				System.out.print(' ');
+			}
+			for(j = 1; j <= i; j++) {
+				System.out.print('*');
+			}
+			for(j = 1; j <= i-1; j++) {
+				System.out.print('*');
+			}
+			System.out.println("");
+		}
+		for(i = 1; i<=4;i++) {
+			for(j = 1; j <= i; j++) {
+				System.out.print(' ');
+			}
+			for(j = 1; j <= 4-i; j++) {
+				System.out.print('*');
+			}
+			for(j = 1; j <= 5-i;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+
+	}
+	
+}
+

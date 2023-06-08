33aa8a7b62f68b4cdf07da246e9e2fdf4ebbe94e
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-08 13:48:04 +0900

    자바 : 7일차 while문 helloworld 예제

---
+++ java/src/day7/breakex/HelloWorld.java
@@ -0,0 +1,16 @@
+package day7.breakex;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while(true) {
+			System.out.println("Hello world!");
+			if(i == 5) {
+				break;
+			}i++;
+		}
+	}
+
+}

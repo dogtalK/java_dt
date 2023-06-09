c5b342a663dae52a87310dd7b34741dec3f084ba
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-09 09:27:34 +0900

    자바 : 7일차 숙제

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -3,7 +3,7 @@ package day7.homework;
 public class StarEx5 {
 
 	public static void main(String[] args) {
-
+/*
 		int i, j;
 		
 		for(i = 1; i <= 5; i++) {
@@ -30,7 +30,32 @@ public class StarEx5 {
 			}
 			System.out.println();
 		}
-
+		*/
+		
+		int num = 7;
+		for(int i = 1; i<=num; i++) {
+			int half = (num + 1)/2;
+			if( i <=half) {
+				for(int j = 1; j <=half-i;j++) {
+					System.out.print(' ');
+				}
+				for(int j = 1;j<=2*i-1;j++) {
+					System.out.print('*');
+				}
+				System.out.println("");
+			}else {
+				for(int j = 1; j <=i-half; j++) {
+					System.out.print(' ');
+				}
+				for(int j = 1; j<=2*(num-i)+1; j++) {
+					System.out.print('*');
+				}
+				System.out.println("");
+			}
+		}
+		
+		
+		
 	}
 	
 }

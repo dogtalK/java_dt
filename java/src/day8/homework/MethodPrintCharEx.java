36c006cab770990b8933a8f8567202dedcfb1157
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-09 15:21:32 +0900

    자바 : 8일차 숙제2

---
+++ java/src/day8/homework/MethodPrintCharEx.java
@@ -0,0 +1,28 @@
+package day8.homework;
+
+public class MethodPrintCharEx {
+
+	public static void main(String[] args) {
+		//다음과 같이 출력하기 위한 코드를 작성하세요
+		//*****
+		//***
+		//?????
+		//^^^^^^^
+		//한줄씩 출력하는 메서드를 만들어라
+
+	}
+	
+	/** 산술 결과를 출력하는 코드
+	 *  매개변수 : 정수, 산술 연산자 => int, char
+	 *  리턴타입 : 정수 => int
+	 *  메서드명 : arith
+	 */
+	public static String asd(char str) {
+		
+		for(int i = 1; i <= num; i++) {
+			System.out.println(str);
+		}
+	}
+	
+	
+}

7698400682e64dd003c3c6a3be82caeb298324f2
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-09 15:21:43 +0900

    자바 : 8일차 숙제4

---
+++ java/src/day8/homework/MethodScoreEx.java
@@ -0,0 +1,35 @@
+package day8.homework;
+
+import java.util.Scanner;
+
+public class MethodScoreEx {
+
+	public static void main(String[] args) {
+		// 성적을 입력받아 학점을 출력하는 코드를 작성하세요.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input month");
+		
+		int score = sc.nextInt();
+		
+		System.out.println(score + " is " + testScore(score));
+	}
+
+	public static String testScore(int score) {
+		String ch = null;
+		if(score < 0 || score > 100) {
+			ch = "wrong score";
+		}else if(score >= 90) {
+			ch = "A";
+		}else if(score >= 80) {
+			ch = "B";
+		}else if(score >= 70) {
+			ch = "C";
+		}else if(score >= 60) {
+			ch = "D";
+		}else {
+			ch = "F";
+		}
+		
+		return ch;
+	}
+}

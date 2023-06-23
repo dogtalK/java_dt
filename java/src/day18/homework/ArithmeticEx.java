994d4c55f3473087ad190f271484efbfc872bc68
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-23 14:54:20 +0900

    자바 18일차 과제

---
+++ java/src/day18/homework/ArithmeticEx.java
@@ -0,0 +1,15 @@
+package day18.homework;
+
+public class ArithmeticEx {
+	/*
+	 * 산술연산을 하는 프로그램을 작성하세요
+	 * 인터페이스 사용
+	 */
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/day18/homework/ImgEx.java
@@ -0,0 +1,18 @@
+package day18.homework;
+
+public class ImgEx {
+	/* 파일명을 입력받아 파일이 이미지인지 확인하는 코드를 작성하세요
+	 * 이미지 확장자 : jpg, bmp, png
+	 * 
+	 * input : dog.jpg
+	 * dog.jpg : Image
+	 * 
+	 * input : dog.exe
+	 * dog.exe : Not image
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

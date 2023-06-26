8a7dabbcac2044fd9242f3bf70364c92aad9be2b
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-06-26 14:53:47 +0900

    자바 : 19일차 과제

---
+++ java/src/day19/homework/ArithmeticEx.java
@@ -0,0 +1,11 @@
+package day19.homework;
+
+public class ArithmeticEx {
+	/*산술 연산하는 메서드를 만들고 테스트라는 코드를 작성하세요 단 예외처리 이용*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

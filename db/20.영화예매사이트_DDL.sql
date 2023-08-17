fc192faffe9cd76edbffd8c0c0f48299bd76dc4a
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-08-17 17:19:54 +0900

    DB : 영화 예매 사이트 - DB 생성 DDL

--- db/20.영화예매사이트_DDL.sql
+++ db/20.영화예매사이트_DDL.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `movie`;
 
 CREATE TABLE `movie` (
 	`mo_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`mo_title`	varchar(50) not null,
+	`mo_title`	varchar(50) NOT NULL,
 	`mo_title_eng`	varchar(100)	NOT NULL,
 	`mo_opening_date`	date	NULL,
 	`mo_running_time`	int	NOT NULL,
@@ -49,8 +49,8 @@ CREATE TABLE `country_production` (
 DROP TABLE IF EXISTS `film_person`;
 
 CREATE TABLE `film_person` (
-	`fp_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`fp_name`	varchar(50)	NOT NULL,
+	`fp_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`fp_name`	varchar(50)	NOT NULL, --
 	`fp_thumbnail`	varchar(50)	NULL,
 	`fp_agency`	varchar(20)	NULL,
 	`fp_final_education`	varchar(20)	NULL,
@@ -61,8 +61,8 @@ CREATE TABLE `film_person` (
 DROP TABLE IF EXISTS `role`;
 
 CREATE TABLE `role` (
-	`ro_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`ro_role`	varchar(30)	NOT NULL,
+	`ro_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`ro_role`	varchar(30)	NOT NULL, --
 	`ro_fp_num`	int	NOT NULL,
 	`ro_mo_num`	int	NOT NULL
 );
@@ -70,15 +70,15 @@ CREATE TABLE `role` (
 DROP TABLE IF EXISTS `file`;
 
 CREATE TABLE `file` (
-	`fi_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`fi_name`	varchar(100)	NOT NULL,
+	`fi_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`fi_name`	varchar(100)	NOT NULL, --
 	`fi_state`	varchar(10)	NOT NULL -- 메인포스트/트레일러/스틸컷 
 );
 
 DROP TABLE IF EXISTS `movie_file`;
 
 CREATE TABLE `movie_file` (
-	`mf_file`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`mf_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
 	`mf_fi_num`	int	NOT NULL,
 	`mf_mo_num`	int	NOT NULL
 );
@@ -86,18 +86,18 @@ CREATE TABLE `movie_file` (
 DROP TABLE IF EXISTS `age`;
 
 CREATE TABLE `age` (
-	`ag_name`	varchar(10)	NOT NULL PRIMARY KEY
+	`ag_name`	varchar(10)	NOT NULL PRIMARY KEY --
 );
 
 DROP TABLE IF EXISTS `theater`;
 
 CREATE TABLE `theater` (
-	`th_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`th_name`	varchar(20)	NOT NULL,
-	`th_address`	varchar(50)	NOT NULL,
-	`th_re_name`	varchar(15)	NOT NULL,
-	`th_total_screen`	int	NOT NULL DEFAULT 1,
-	`th_total_seat`	int	NOT NULL
+	`th_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`th_name`	varchar(20)	NOT NULL, --
+	`th_address`	varchar(50)	NOT NULL, --
+	`th_re_name`	varchar(15)	NOT NULL, 
+	`th_total_screen`	int	NOT NULL DEFAULT 1, --
+	`th_total_seat`	int	NOT NULL --
 );
 
 DROP TABLE IF EXISTS `region`;
@@ -109,62 +109,62 @@ CREATE TABLE `region` (
 DROP TABLE IF EXISTS `screen`;
 
 CREATE TABLE `screen` (
-	`sc_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`sc_name`	varchar(20)	NOT NULL,
-	`sc_total_seat`	int	NOT NULL,
+	`sc_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`sc_name`	varchar(20)	NOT NULL, --
+	`sc_total_seat`	int	NOT NULL, -- 
 	`sc_th_num`	int	NOT NULL
 );
 
 DROP TABLE IF EXISTS `seat`;
 
 CREATE TABLE `seat` (
-	`se_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`se_name`	varchar(4)	NOT NULL,
-	`se_row`	char(1)	NOT NULL,
-	`se_col`	varchar(2)	NOT NULL,
-	`se_state`	varchar(10)	NOT NULL DEFAULT '일반', -- 일반/커플/스위트
+	`se_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`se_name`	varchar(4)	NOT NULL, --
+	`se_row`	char(1)	NOT NULL, --
+	`se_col`	varchar(2)	NOT NULL, --
+	`se_state`	varchar(10)	NOT NULL DEFAULT '일반', -- 일반/커플/스위트 
 	`se_sc_num`	int	NOT NULL
 );
 
 DROP TABLE IF EXISTS `movie_schedule`;
 
 CREATE TABLE `movie_schedule` (
-	`ms_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`ms_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
 	`ms_mo_num`	int	NOT NULL,
 	`ms_sc_num`	int	NOT NULL,
-	`ms_date`	date	NOT NULL,
-	`ms_start_time`	time	NOT NULL,
-	`ms_end_time`	time	NOT NULL,
-	`ms_possible_seat`	int	NOT NULL,
-	`ms_discount`	char(1)	NOT NULL DEFAULT 'N'
+	`ms_date`	date	NOT NULL,--
+	`ms_start_time`	time	NOT NULL,--
+	`ms_end_time`	time	NOT NULL,--
+	`ms_possible_seat`	int	NOT NULL,--
+	`ms_discount`	char(1)	NOT NULL DEFAULT 'N'--
 );
 
 DROP TABLE IF EXISTS `member`;
 
 CREATE TABLE `member` (
-	`me_id`	varchar(15)	NOT NULL PRIMARY KEY,
-	`me_pw`	varchar(20)	NOT NULL,
-	`me_name`	varchar(20)	NOT NULL,
-	`me_phone`	varchar(15)	NOT NULL,
-	`me_birthday`	date	NOT NULL,
+	`me_id`	varchar(15)	NOT NULL PRIMARY KEY, --
+	`me_pw`	varchar(20)	NOT NULL,--
+	`me_name`	varchar(20)	NOT NULL,--
+	`me_phone`	varchar(15)	NOT NULL,--
+	`me_birthday`	date	NOT NULL,--
     `me_authority` varchar(5) NOT NULL DEFAULT 'USER'
 );
 
 DROP TABLE IF EXISTS `reservation`;
 
 CREATE TABLE `reservation` (
-	`rv_num`	varchar(20)	NOT NULL PRIMARY KEY,
-	`rv_adult`	int	NOT NULL DEFAULT 0,
-	`rv_me_id`	varchar(15)	NOT NULL,
+	`rv_num`	varchar(20)	NOT NULL PRIMARY KEY,--
+	`rv_adult`	int	NOT NULL DEFAULT 0,--
 	`rv_ms_num`	int	NOT NULL,
-	`rv_teenager`	int	NOT NULL DEFAULT 0,
-	`rv_price`	int	NOT NULL
+	`rv_me_id`	varchar(15)	NOT NULL,
+	`rv_teenager`	int	NOT NULL DEFAULT 0,--
+	`rv_price`	int	NOT NULL--
 );
 
 DROP TABLE IF EXISTS `reservation_list`;
 
 CREATE TABLE `reservation_list` (
-	`rl_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`rl_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,--
 	`rl_rv_num`	varchar(20)	NOT NULL,
 	`rl_se_num`	int	NOT NULL,
 	`rl_pr_num`	int	NOT NULL
@@ -173,7 +173,7 @@ CREATE TABLE `reservation_list` (
 DROP TABLE IF EXISTS `price`;
 
 CREATE TABLE `price` (
-	`pr_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`pr_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
 	`pr_type`	varchar(3)	NOT NULL,
 	`pr_price`	int	NOT NULL,
 	`pr_discount_price`	int	NOT NULL
@@ -182,19 +182,19 @@ CREATE TABLE `price` (
 DROP TABLE IF EXISTS `review`;
 
 CREATE TABLE `review` (
-	`re_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`re_content`	longtext	NOT NULL,
+	`re_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,--
+	`re_content`	longtext	NOT NULL,--
 	`re_mo_num`	int	NOT NULL,
 	`re_me_id`	varchar(15)	NOT NULL,
-	`re_total_like`	int	NOT NULL DEFAULT 0
+	`re_total_like`	int	NOT NULL DEFAULT 0 --
 );
 
 DROP TABLE IF EXISTS `like`;
 
 CREATE TABLE `like` (
-	`li_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`li_me_id`	varchar(15)	NOT NULL,
-	`li_re_num`	int	NOT NULL
+	`li_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT, --
+	`me_id`	varchar(15)	NOT NULL,
+	`re_num`	int	NOT NULL
 );
 
 ALTER TABLE `movie` ADD CONSTRAINT `FK_file_TO_movie_1` FOREIGN KEY (
@@ -309,18 +309,18 @@ REFERENCES `screen` (
 	`sc_num`
 );
 
-ALTER TABLE `reservation` ADD CONSTRAINT `FK_member_TO_reservation_1` FOREIGN KEY (
-	`rv_me_id`
-)
-REFERENCES `member` (
-	`me_id`
-);
-
 ALTER TABLE `reservation` ADD CONSTRAINT `FK_movie_schedule_TO_reservation_1` FOREIGN KEY (
 	`rv_ms_num`
 )
 REFERENCES `movie_schedule` (
 	`ms_num`
+);
+
+ALTER TABLE `reservation` ADD CONSTRAINT `FK_member_TO_reservation_1` FOREIGN KEY (
+	`rv_me_id`
+)
+REFERENCES `member` (
+	`me_id`
 );
 
 ALTER TABLE `reservation_list` ADD CONSTRAINT `FK_reservation_TO_reservation_list_1` FOREIGN KEY (
@@ -359,14 +359,14 @@ REFERENCES `member` (
 );
 
 ALTER TABLE `like` ADD CONSTRAINT `FK_member_TO_like_1` FOREIGN KEY (
-	`li_me_id`
+	`me_id`
 )
 REFERENCES `member` (
 	`me_id`
 );
 
 ALTER TABLE `like` ADD CONSTRAINT `FK_review_TO_like_1` FOREIGN KEY (
-	`li_re_num`
+	`re_num`
 )
 REFERENCES `review` (
 	`re_num`

480973bbb5c62952fa02b76c23a27617a8d33a02
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-09-07 17:54:35 +0900

    세미프로젝트 DDL,샘플데이터

--- db/세미프로젝트_인사관리DDL.sql
+++ db/세미프로젝트_인사관리DDL.sql
@@ -14,8 +14,8 @@ CREATE TABLE `employee` (
 	`ep_dm_num`	INT	NOT NULL DEFAULT 1,
 	`ep_po_num`	INT	NOT NULL DEFAULT 1,
 	`ep_st_num`	int	NOT NULL DEFAULT 1,
-	`ep_salay`	VARCHAR(255)	NOT NULL,
-	`ep_leave`	int	NOT NULL DEFAULT 15 
+	`ep_leave`	int	NOT NULL DEFAULT 15,
+    `ep_salay`	VARCHAR(255)	NOT NULL
 );
 
 DROP TABLE IF EXISTS `department`;
@@ -48,7 +48,8 @@ CREATE TABLE `salaryHistory` (
 	`sh_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`sh_payday`	DATE	NOT NULL,
 	`sh_ep_id`	INT	NOT NULL,
-	`sh_bo_type`	VARCHAR(20)	NOT NULL,
+	`sh_pa_type`	VARCHAR(20)	NOT NULL,
+    `sh_type_detail`	VARCHAR(20)	NOT NULL,
 	`sh_salary`	INT	NOT NULL
 );
 
@@ -57,8 +58,8 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
 	`pj_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`pj_name`	VARCHAR(255)	NOT NULL,
-	`pj_start_date`	DATE	NOT NULL,
-	`pj_end_date`	DATE	NOT NULL,
+	`pj_start_date`	DATE	NULL,
+	`pj_end_date`	DATE	NULL,
 	`pj_state`	VARCHAR(15)	NOT NULL
 );
 
@@ -66,34 +67,31 @@ DROP TABLE IF EXISTS `Termination`;
 
 CREATE TABLE `Termination` (
 	`tm_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`tm_date`	DATE	NULL,
-	`tm_retrun_date`	DATE	NULL,
+	`tm_date`	DATE	NOT NULL,
+	`tm_return_date`	DATE	NULL,
 	`tm_reason`	VARCHAR(30)	NOT NULL,
 	`tm_ep_id`	INT	NOT NULL
 );
 
-DROP TABLE IF EXISTS `attendance`;
+DROP TABLE IF EXISTS `at_record`;
 
-CREATE TABLE `attendance` (
-	`ad_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`ad_st_time`	TIME	NULL,
-	`ad_end_time`	TIME	NULL,
-	`ad_ad_date`	DATE	NULL,
-	`ad_at_num`	INT	NOT NULL,
-	`ad_ep_id`	INT	NOT NULL
+CREATE TABLE `at_record` (
+	`ar_ad_num`	INT	NOT NULL,
+	`ar_st_time`	TIME	NULL,
+	`ar_end_time`	TIME	NULL
 );
 
 DROP TABLE IF EXISTS `attendance_type`;
 
 CREATE TABLE `attendance_type` (
 	`at_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`at_type`	VARCHAR(15)	NULL
+	`at_type`	VARCHAR(15)	NOT NULL
 );
 
 DROP TABLE IF EXISTS `project_info`;
 
 CREATE TABLE `project_info` (
-	`pi_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`pi_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`pi_ep_id`	INT	NOT NULL,
 	`pi_pj_num`	INT	NOT NULL,
 	`pi_role`	VARCHAR(20)	NOT NULL
@@ -102,16 +100,16 @@ CREATE TABLE `project_info` (
 DROP TABLE IF EXISTS `status`;
 
 CREATE TABLE `status` (
-	`st_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`st_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`st_type`	VARCHAR(20)	NOT NULL
 );
 
 DROP TABLE IF EXISTS `transfer`;
 
 CREATE TABLE `transfer` (
-	`tr_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`tr_reason`	VARCHAR(30)	NOT NULL,
-	`tr_date`	date	NOT NULL,
+	`tr_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`tr_date`	DATE	NOT NULL,
+    `tr_reason`	VARCHAR(30)	NOT NULL,
 	`tr_ep_id`	INT	NOT NULL,
 	`tr_dm_num`	INT	NOT NULL
 );
@@ -120,6 +118,15 @@ DROP TABLE IF EXISTS `pay`;
 
 CREATE TABLE `pay` (
 	`pa_type`	VARCHAR(20)	NOT NULL PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS `attendance`;
+
+CREATE TABLE `attendance` (
+	`ad_num`	INT	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`ad_date`	DATE	NOT NULL,
+    `ad_ep_id`	INT	NOT NULL,
+    `ad_at_num`	INT	NOT NULL DEFAULT 1
 );
 
 ALTER TABLE `employee` ADD CONSTRAINT `FK_department_TO_employee_1` FOREIGN KEY (
@@ -158,7 +165,7 @@ REFERENCES `employee` (
 );
 
 ALTER TABLE `salaryHistory` ADD CONSTRAINT `FK_pay_TO_salaryHistory_1` FOREIGN KEY (
-	`sh_bo_type`
+	`sh_pa_type`
 )
 REFERENCES `pay` (
 	`pa_type`
@@ -171,18 +178,11 @@ REFERENCES `employee` (
 	`ep_id`
 );
 
-ALTER TABLE `attendance` ADD CONSTRAINT `FK_attendance_type_TO_attendance_1` FOREIGN KEY (
-	`ad_at_num`
+ALTER TABLE `at_record` ADD CONSTRAINT `FK_attendance_TO_at_record_1` FOREIGN KEY (
+	`ar_ad_num`
 )
-REFERENCES `attendance_type` (
-	`at_num`
-);
-
-ALTER TABLE `attendance` ADD CONSTRAINT `FK_employee_TO_attendance_1` FOREIGN KEY (
-	`ad_ep_id`
-)
-REFERENCES `employee` (
-	`ep_id`
+REFERENCES `attendance` (
+	`ad_num`
 );
 
 ALTER TABLE `project_info` ADD CONSTRAINT `FK_employee_TO_project_info_1` FOREIGN KEY (
@@ -211,5 +211,19 @@ ALTER TABLE `transfer` ADD CONSTRAINT `FK_department_TO_transfer_1` FOREIGN KEY 
 )
 REFERENCES `department` (
 	`dm_num`
+);
+
+ALTER TABLE `attendance` ADD CONSTRAINT `FK_attendance_type_TO_attendance_1` FOREIGN KEY (
+	`ad_at_num`
+)
+REFERENCES `attendance_type` (
+	`at_num`
+);
+
+ALTER TABLE `attendance` ADD CONSTRAINT `FK_employee_TO_attendance_1` FOREIGN KEY (
+	`ad_ep_id`
+)
+REFERENCES `employee` (
+	`ep_id`
 );
 

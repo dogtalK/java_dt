130bd93c962873ce7d12e6b4a876a0f4d8a33fe1
dogtalK <134986660+dogtalK@users.noreply.github.com>
2023-09-21 16:12:44 +0900

    세미프로젝트 수정

--- java/src/pmproject/vo/Project_infoVO.java
+++ java/src/pmproject/vo/Project_infoVO.java
@@ -1,12 +1,10 @@
 package pmproject.vo;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project_infoVO {
